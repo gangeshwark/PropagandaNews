a83c47d77f6748a13a914f3f85147b54b59312b7
gangeshwark <gangeshwark@gmail.com>
2019-11-07 12:22:18 +0800

    Bug fix

--- train_tc.py
+++ train_tc.py
@@ -43,6 +43,22 @@ def create_split():
     pass
 
 
+index2label = {
+    0: 'Appeal_to_Authority',
+    1: 'Appeal_to_fear-prejudice',
+    2: 'Bandwagon,Reductio_ad_hitlerum',
+    3: 'Black-and-White_Fallacy',
+    4: 'Causal_Oversimplification',
+    5: 'Doubt',
+    6: 'Exaggeration,Minimisation',
+    7: 'Flag-Waving',
+    8: 'Loaded_Language',
+    9: 'Name_Calling,Labeling',
+    10: 'Repetition',
+    11: 'Slogans',
+    12: 'Thought-terminating_Cliches',
+    13: 'Whataboutism,Straw_Men,Red_Herring'
+}
 if __name__ == '__main__':
     train_x, train_seq_len, train_y, dev_x, dev_seq_len, embeddings = load_data()
 
@@ -66,6 +82,7 @@ if __name__ == '__main__':
     model.fit(train_x, train_y, validation_data=[test_x, test_y], epochs=5, batch_size=256, shuffle=True)
 
     predictions = model.predict(dev_x)
+    predictions = predictions.argmax(axis=1)
 
     # writing predictions to file
     task_TC_output_file = "model-output-TC.txt"
@@ -74,5 +91,5 @@ if __name__ == '__main__':
     with open(task_TC_output_file, "w") as fout:
         for article_id, prediction, span_start, span_end in zip(dev_article_ids, predictions, dev_span_starts,
                                                                 dev_span_ends):
-            fout.write("%s\t%s\t%s\t%s\n" % (article_id, prediction, span_start, span_end))
+            fout.write("%s\t%s\t%s\t%s\n" % (article_id, index2label[prediction], span_start, span_end))
     print("Predictions written to file " + task_TC_output_file)
